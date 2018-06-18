0e6fada1156fe26caacc13bf4713d787cbcd50a6
jdpigeon <morrisondano@gmail.com>
2018-06-18 14:55:43 -0400

    🚀

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# emotiv-observable
+# Emotiv Observable
 Making Emotiv Cortex SDK for Node Reactive
+
+*Note: this repo is a work in progress*
+
+This is a simple example of how to create an RxJS observable of EEG data from an Emotiv Epoc headset using the Cortex SDK. The data format produced by the `createRawEmotivObservable` function is identical to that of [OpenBCI Observable](https://github.com/neurosity/openbci-observable) and [Muse JS](https://github.com/urish/muse-js) and is suitable for use with [EEG Pipes](https://github.com/neurosity/eeg-pipes). Eventually, this might be tightened up into a package that will make it easier for JS developers to get started 
+
+## Authentication
+**An active license from Emotiv is necessary to access raw data**
+
+Licenses are tied to specific EmotivID accounts. To authenticate and access raw data with an active license you must follow these steps:
+1. Ensure you have an EmotivID account with an active subscription for raw data access
+2.  Download and install the Cortex SDK from Emotiv's website
+3. Go to the 'Cortex Apps' section of your account page and generate a 'Client ID' and 'Client Secret'
+4. Fill in the values for `username`, `password`, `client_id`, and `client_secret` in raw.js
---
+++ src/raw.js
@@ -0,0 +1,53 @@
+/*
+ * Adapted from the Cortex example, this will return an RxJS observable of raw EEG data
+ *
+ */
+
+const Cortex = require("../lib/cortex");
+const { from, fromEvent } = require("rxjs");
+const { mergeMap, map, tap } = require("rxjs/operators");
+
+function createRawEmotivObservable(client, auth, onEEG) {
+  return from(
+    client.ready
+      .then(() => client.init(auth))
+      .then(() =>
+        client
+          .createSession({ status: "active" })
+          .subscribe({ streams: ["eeg"] })
+      )
+  ).pipe(
+    mergeMap(subs => {
+      if (!subs[0].eeg) throw new Error("failed to subscribe");
+      return fromEvent(client, "eeg").pipe(map(onEEG));
+    })
+  );
+}
+
+if (require.main === module) {
+  process.on("unhandledRejection", err => {
+    throw err;
+  });
+
+  const verbose = process.env.LOG_LEVEL || 1;
+  const options = { verbose };
+
+  const client = new Cortex(options);
+  // these values need to be filled to run example
+  const auth = {
+    username: "...",
+    password: "...",
+    client_id: "...",
+    client_secret: "...",
+    debit: 1
+  };
+
+  const onEEG = data => ({
+    data: data.eeg,
+    timestamp: data.time
+  });
+
+  rawObservable = createRawEmotivObservable(client, auth, onEEG);
+
+  rawObservable.subscribe(console.log);
+}
